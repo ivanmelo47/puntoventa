4f1a255bd6bce6e8918bdc0e5d40831a37be7ccb
ivanmelo47 <ivanmelo47@gmail.com>
2024-03-03 22:23:43 -0600

    new2

---
+++ resources/views/almacen/categoria/create.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.admin')
+@section('contenido')
+
+<h3>Este es el Create-Categoria</h3>
+
+@stop
---
+++ resources/views/almacen/categoria/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.admin')
+@section('contenido')
+
+<h3>Este es el Index</h3>
+
+@stop
